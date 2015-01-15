9fd1c73a4f81910ba43f06a9ee3db822e2b63d49
10031066 <edgar_5_11@hotmail.com>
2015-01-15 08:26:06 -0600

    Equipos

--- path/src/limubac/administratorBundle/Controller/DefaultController.php
+++ path/src/limubac/administratorBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace limubac\administratorBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use limubac\administratorBundle\Entity\equipo;
 
 class DefaultController extends Controller
 {
@@ -19,7 +20,41 @@ class DefaultController extends Controller
     public function hojaAnotacionesAction(){
     	return $this->render('limubacadministratorBundle:administracion:hojaAnotaciones.html.twig');
     }
+	//Edgar
+	
 	public function equiposAction(){
-    	return $this->render('limubacadministratorBundle:administracion:equipos.html.twig');
+		if(isset($_POST['NuevoEquipo'])){
+			$equipo = new equipo();
+			$equipo->setNombre($_POST['NuevoEquipo']);
+			
+			$Manager = $this->getDoctrine()->getManager();
+			$Manager->persist($equipo);
+			$Manager->flush();
+		}
+		$repositorio = $this->getDoctrine()->getRepository("limubacadministratorBundle:Equipo");
+		
+		$ListaEquipos = $repositorio->findAll();
+    	return $this->render('limubacadministratorBundle:administracion:equipos.html.twig', array('listEquip' =>$ListaEquipos));
     }
+	
+	public function equipoAction(){
+		if(isset($_POST['opciones'])){
+		
+		$Equi = array(
+			'NumEquipo' =>$_POST['opciones'],
+			'NomEquipo' =>'valedores',
+			'Capitan'=>'',
+			'Representante'=>'',
+			'Auxiliar'=>'',
+			$jugadores = array(
+				array("numero"=>1,"nombre"=>"Pablo")
+			)
+		);
+		return $this->render('limubacadministratorBundle:administracion:equipo.html.twig',array('equipo'=>$Equi));
+		
+		}else{//si no esta definido el valor del equipo
+		
+		}
+	}
+	//End Edgar
 }
