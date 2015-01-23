bb2d24c04cc5e2264970bf346ff84101978bfc97
faficarreon <faficarreon@gmail.com>
2015-01-23 03:53:11 -0600

    Actualizacion para Edgar :3

---
+++ path/src/limubac/administratorBundle/Entity/Fotos.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace limubac\administratorBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Fotos
+ *
+ * @ORM\Table(name="fotos")
+ * @ORM\Entity
+ */
+class Fotos
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=255, nullable=false)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="foto", type="blob", nullable=false)
+     */
+    private $foto;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id_foto", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idFoto;
+
+
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     * @return Fotos
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set foto
+     *
+     * @param string $foto
+     * @return Fotos
+     */
+    public function setFoto($foto)
+    {
+        $this->foto = $foto;
+
+        return $this;
+    }
+
+    /**
+     * Get foto
+     *
+     * @return string 
+     */
+    public function getFoto()
+    {
+        return $this->foto;
+    }
+
+    /**
+     * Get idFoto
+     *
+     * @return integer 
+     */
+    public function getIdFoto()
+    {
+        return $this->idFoto;
+    }
+}
