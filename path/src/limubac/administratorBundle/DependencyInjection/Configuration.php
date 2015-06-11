a874496855b46e0da5c421b59abbfc624ae5e7ce
faficarreon <faficarreon@gmail.com>
2015-06-11 13:17:58 -0500

    Actulizacion documentos/base

--- path/src/limubac/administratorBundle/DependencyInjection/Configuration.php
+++ path/src/limubac/administratorBundle/DependencyInjection/Configuration.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace limubac\administratorBundle\DependencyInjection;
-
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
-
 /**
  * This is the class that validates and merges configuration from your app/config files
  *
@@ -19,11 +16,9 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('limubacadministrator');
-
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
-
         return $treeBuilder;
     }
-}
+}   
