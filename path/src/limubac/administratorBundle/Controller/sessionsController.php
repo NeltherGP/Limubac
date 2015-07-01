8a8015e50a48b0bef43df848662d99be1436f009
Erick Ricardo Corona Rosales <crerick19@gmail.com>
2015-06-30 23:56:10 -0500

    avance de sesion

--- path/src/limubac/administratorBundle/Controller/sessionsController.php
+++ path/src/limubac/administratorBundle/Controller/sessionsController.php
@@ -1,6 +1,7 @@
 <?php
 	namespace limubac\administratorBundle\Controller;
 
+
 	//use limubac\administratorBundle\Resources\views\administracion\sesiones;
 
 	use limubac\administratorBundle\Controller\SesionesConsultas;
@@ -13,13 +14,15 @@
 	use Symfony\Component\HttpFoundation\Session\sfAction;
 
 	class sessionsController extends Controller{
+		
+
  		public function logAction(){
+ 			$count=0;
 			$request = $this->getRequest();
 	        $session = $request->getSession();
-
 	        // obtiene el error de inicio de sesión si lo hay
 	        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-	        	echo "ERROR 1";
+	        	//echo "ERROR 1";
 	            $error = $request->attributes->get(
 	                SecurityContext::AUTHENTICATION_ERROR
 	            );
@@ -31,12 +34,10 @@
 	            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
 	            //print_r($error);
 	        }
-
-	        //echo "ERROR 3";
-			      
-	        //$usr=$this->getUser();
-	        //print_r($usr);
-	               	return $this->render(
+	        //echo "ERROR 3--";
+	       print_r($error);
+	        
+	        	return $this->render(
 		            'limubacadministratorBundle:administracion:sesiones.html.twig',
 		            array(
 		                // último nombre de usuario ingresado
@@ -44,18 +45,62 @@
 		                'error'         => $error
 		            )
 	        	);
+	        
+			 			
 		}
 	
-		public function actAction(){
+		public function newuserAction(){
+
+			if (isset($_POST["confirmar_password"]) && isset($_POST["nuevo_password"])) {
+				$clave1 = $_POST["confirmar_password"];
+   				$clave2 = $_POST["nuevo_password"];
+
+   				$validado = "Registro exitoso";
+   				$novalidado = "Error, contraseñas no coinciden";
+
+   				if ($clave1 == $clave2) {
+   					return $this->render(
+		            'limubacadministratorBundle:administracion:adminPanel.html.twig',  array('mensaje' => $validado)
+	        		);
+		      		alert("Las dos claves son iguales...\nRealizaríamos las acciones del caso positivo"); 
+			   	}
+			   	else {
+			   		return $this->render(
+		            'limubacadministratorBundle:administracion:NuevoUsuario.html.twig', array('mensaje' => $novalidado)
+	        		);
+			      	alert("Las dos claves son distintas...\nRealizaríamos las acciones del caso negativo"); 
+			   	}
+			}else{
+				return $this->render(
+		            'limubacadministratorBundle:administracion:NuevoUsuario.html.twig'
+	        	);
+			}
+			
+		}
+
+		public function actualizarContrasenaAction(){
+			
 			return $this->render(
-		            'limubacadministratorBundle:administracion:ActualizarSesiones.html.twig'
+		            'limubacadministratorBundle:administracion:actContrasena.html.twig'
 	        	);
 		}
+			
+			
+		
 
 		public function logoutAction(){
 			return $this->render(
 		            'limubacadministratorBundle:administracion:logout.html.twig'
 	        	);
 		}
+
+
+		public function miPerfilAction(){
+			return $this->render(
+		            'limubacadministratorBundle:administracion:perfin.html.twig'
+	        	);
+		}
+
+		
 	}
 ?>
