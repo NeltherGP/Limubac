52a1fbd63baf13e6b38f4969d105fe785ec0ff78
Erick Ricardo Corona Rosales <crerick19@gmail.com>
2015-07-21 13:51:00 -0500

    Lista de usuarios completa

---
+++ path/src/limubac/administratorBundle/Form/Type/UserType.php
@@ -0,0 +1,35 @@
+<?php
+//src/Acme/TaskBundle/Form/Type/JugadorType.php
+namespace limubac\administratorBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType{
+	public function buildForm(FormBuilderInterface $builder, array $options){
+		$builder->add('id', 'hidden');
+		$builder->add('username', 'text');
+		$builder->add('password','password');
+		$builder->add('email','email');
+		$builder->add('isActive','text');
+		$builder->add('address','text');		
+		$builder->add('phone','integer');
+		$builder->add('roles','text');
+		$builder->add('name','text');
+		//$builder->add('inscripcionAbierta','integer');
+	}
+
+	public function getName(){
+		return 'user';
+	}
+
+	/*public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+    $resolver->setDefaults(array(
+        'data_class' => 'limubac\administratorBundle\Entity\Jugador',
+    ));
+    }*/
+}
+?>
